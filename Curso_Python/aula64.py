63a7b38ade8099ba0c1cfdbde3269216c68ba8f8
João Guilherme <joaog99.cotrim@gmail.com>
2025-02-25 16:28:08 -0300

    Estudo dia 25/02/2025

---
+++ Curso_Python/aula64.py
@@ -0,0 +1,22 @@
+"""
+Introdução às funções (def) em Python
+Funções são trechso de códigos usados para 
+replicar determinda ações ao longo do seu código.
+Elas podem receber calores para parâmestros (argumentos)
+e retornar um valor específico.
+Por padrão, funções Python retornam Nome (nada).
+"""
+
+def Print():
+    print('Várias1')
+    print('Várias2')
+    print('Várias3')
+    print('Várias4')
+    print('Várias5')
+
+Print()
+Print()
+Print()
+Print()
+Print()
+Print()
